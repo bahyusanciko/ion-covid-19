2921f21518ebc8863f26de7fc59e2808425ed4fa
unknown <cbahyu@gmail.com>
2021-05-26 08:59:36 +0700

    fix code capactitor

--- ion-covid/src/definitions.ts
+++
@@ -1,9 +0,0 @@
-declare module "@capacitor/core" {
-  interface PluginRegistry {
-    IonCovid: IonCovidPlugin;
-  }
-}
-
-export interface IonCovidPlugin {
-  echo(options: { value: string }): Promise<{value: string}>;
-}
